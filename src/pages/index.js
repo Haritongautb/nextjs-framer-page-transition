a6e91320c20ca3eefe90f216b234e6e0d4ed9971
Haritongautb <khariton.aaa.97@gmail.com>
2024-04-04 13:11:04 +0200

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
-import Head from 'next/head'
-import Curve from '@/components/Layout/Curve'
+import Head from "next/head";
+import Curve from "@/components/Layout/Curve";
 
 export default function Home() {
   return (
@@ -12,11 +12,32 @@ export default function Home() {
       </Head>
       <Curve backgroundColor={"#B0AD98"}>
         <h1>Home</h1>
-        <div className='body'>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent imperdiet nibh sit amet velit dignissim, non tempus nisl pellentesque. Praesent sagittis magna sit amet ex blandit, id pharetra lectus feugiat. Praesent sit amet congue ipsum, in ultrices neque. In dapibus in purus vitae dignissim. Quisque molestie ullamcorper elementum. Sed sodales erat augue. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis aliquet quis lectus vitae venenatis. Aliquam erat volutpat. Nulla maximus sodales nibh dapibus congue. Integer nec pharetra felis, quis commodo elit. Fusce et aliquet neque. Vivamus leo diam, pharetra ut lorem eu, suscipit egestas ipsum. Aenean mauris ligula, laoreet ut volutpat sit amet, convallis et turpis.</p>
-            <p>Quisque molestie ullamcorper elementum. Sed sodales erat augue. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis aliquet quis lectus vitae venenatis. Aliquam erat volutpat. Nulla maximus sodales nibh dapibus congue. Integer nec pharetra felis, quis commodo elit. Fusce et aliquet neque. Vivamus leo diam, pharetra ut lorem eu, suscipit egestas ipsum. Aenean mauris ligula, laoreet ut volutpat sit amet, convallis et turpis.</p>
+        <div className="body">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
+            imperdiet nibh sit amet velit dignissim, non tempus nisl
+            pellentesque. Praesent sagittis magna sit amet ex blandit, id
+            pharetra lectus feugiat. Praesent sit amet congue ipsum, in ultrices
+            neque. In dapibus in purus vitae dignissim. Quisque molestie
+            ullamcorper elementum. Sed sodales erat augue. Lorem ipsum dolor sit
+            amet, consectetur adipiscing elit. Duis aliquet quis lectus vitae
+            venenatis. Aliquam erat volutpat. Nulla maximus sodales nibh dapibus
+            congue. Integer nec pharetra felis, quis commodo elit. Fusce et
+            aliquet neque. Vivamus leo diam, pharetra ut lorem eu, suscipit
+            egestas ipsum. Aenean mauris ligula, laoreet ut volutpat sit amet,
+            convallis et turpis.
+          </p>
+          <p>
+            Quisque molestie ullamcorper elementum. Sed sodales erat augue.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
+            aliquet quis lectus vitae venenatis. Aliquam erat volutpat. Nulla
+            maximus sodales nibh dapibus congue. Integer nec pharetra felis,
+            quis commodo elit. Fusce et aliquet neque. Vivamus leo diam,
+            pharetra ut lorem eu, suscipit egestas ipsum. Aenean mauris ligula,
+            laoreet ut volutpat sit amet, convallis et turpis.
+          </p>
         </div>
       </Curve>
     </>
-  )
+  );
 }
